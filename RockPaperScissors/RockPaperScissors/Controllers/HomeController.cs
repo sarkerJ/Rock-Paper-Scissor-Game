e5cbbd7446fede602e13c7155a4645243bdca705
SarkerJ <isarkerj@gmail.com>
2021-01-05 12:01:41 +0000

    Added buttons to homepage
    Implemented two buttons which direct you to two different pages.
    
    Currently the start button simply directs you to the Privacy page, this was a test and its temporary until the game page is created

--- RockPaperScissors/RockPaperScissors/Controllers/HomeController.cs
+++ RockPaperScissors/RockPaperScissors/Controllers/HomeController.cs
@@ -26,9 +26,15 @@ namespace RockPaperScissors.Controllers
         public IActionResult Privacy()
         {
             return View();
+
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Instructions()
+        {
+            return View();
+        }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
